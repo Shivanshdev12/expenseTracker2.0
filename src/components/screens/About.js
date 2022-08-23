17d897df740b873cc4824d6fe0b03e10bf9a2300
Shivansh Mehrotra <shivansh.sm1@gmail.com>
2022-08-23 18:54:05 +0530

    Login/Signup update

--- src/components/screens/About.js
+++ src/components/screens/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import homeLogoTodo from "../../homeLogotodo.svg";
 import "./About.css";
 
 const About = () => {
@@ -8,6 +9,7 @@ const About = () => {
         <section className={bgColor === false ? "about" : "about about_dark"}>
             <div className="heading">
                 <h1>About Us</h1>
+                <img src={homeLogoTodo} alt="logo" />
             </div>
             <div className="main">
                 <p>I'm Shivansh Mehrotra. B.tech. graduate from JSS Academy of Technical education, Noida.
